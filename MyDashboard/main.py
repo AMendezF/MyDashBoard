b3bd45520b4444f92dbdcfae4ec7ad5eace18247
AMendezF <a.mendezwork@gmail.com>
2018-05-06 21:04:22 +0200

    Cleaning and fixing aspect

--- MyDashboard/main.py
+++ MyDashboard/main.py
@@ -1,11 +1,19 @@
+import kivy
+kivy.require('1.10.0')
+
 from kivy.config import Config
 Config.set('input', 'mouse', 'mouse,disable_multitouch')
+Config.set('graphics', 'minimum_width', '1280')
+Config.set('graphics', 'minimum_height', '720')
+Config.set('graphics', 'width', '1280')
+Config.set('graphics', 'height', '720')
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
-from kivy.properties import BooleanProperty
+from kivy.properties import BooleanProperty, ListProperty
 
 class MyDashboardScreen(Screen):
     fullscreen = BooleanProperty(False)
+    statusColor = ListProperty([0, .9, 0, 1])
 
     def add_widget(self, *args):
         if 'content' in self.ids:
@@ -15,7 +23,7 @@ class MyDashboardScreen(Screen):
 class MyDashboardApp(App):
 
     def build(self):
-        self.title = 'hello world'
+        self.title = 'MyDashboard'
         return MyDashboardScreen()
 
 if __name__ == '__main__':
